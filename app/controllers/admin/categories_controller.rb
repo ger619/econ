bc7c1cf9d54785307391bb40f7f6a6423864fc53
ger619 <draker74@gmail.com>
2024-05-27 16:00:39 +0300

    adding categories without linters

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < AdminController
-  before_action :set_admin_category, only: %i[ show edit update destroy ]
+  before_action :set_admin_category, only: %i[show edit update destroy]
 
   # GET /admin/categories or /admin/categories.json
   def index
@@ -7,8 +7,7 @@ class Admin::CategoriesController < AdminController
   end
 
   # GET /admin/categories/1 or /admin/categories/1.json
-  def show
-  end
+  def show; end
 
   # GET /admin/categories/new
   def new
@@ -16,8 +15,7 @@ class Admin::CategoriesController < AdminController
   end
 
   # GET /admin/categories/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /admin/categories or /admin/categories.json
   def create
@@ -25,7 +23,7 @@ class Admin::CategoriesController < AdminController
 
     respond_to do |format|
       if @admin_category.save
-        format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully created." }
+        format.html { redirect_to admin_category_url(@admin_category), notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @admin_category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +36,7 @@ class Admin::CategoriesController < AdminController
   def update
     respond_to do |format|
       if @admin_category.update(admin_category_params)
-        format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully updated." }
+        format.html { redirect_to admin_category_url(@admin_category), notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @admin_category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +50,20 @@ class Admin::CategoriesController < AdminController
     @admin_category.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_categories_url, notice: "Category was successfully destroyed." }
+      format.html { redirect_to admin_categories_url, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_category
-      @admin_category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def admin_category_params
-      params.require(:admin_category).permit(:name, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_admin_category
+    @admin_category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def admin_category_params
+    params.require(:admin_category).permit(:name, :description)
+  end
 end
--- test/system/admin/categories_test.rb
+++ test/system/admin/categories_test.rb
@@ -1,43 +1,43 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class Admin::CategoriesTest < ApplicationSystemTestCase
   setup do
     @admin_category = admin_categories(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit admin_categories_url
-    assert_selector "h1", text: "Categories"
+    assert_selector 'h1', text: 'Categories'
   end
 
-  test "should create category" do
+  test 'should create category' do
     visit admin_categories_url
-    click_on "New category"
+    click_on 'New category'
 
-    fill_in "Description", with: @admin_category.description
-    fill_in "Name", with: @admin_category.name
-    click_on "Create Category"
+    fill_in 'Description', with: @admin_category.description
+    fill_in 'Name', with: @admin_category.name
+    click_on 'Create Category'
 
-    assert_text "Category was successfully created"
-    click_on "Back"
+    assert_text 'Category was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Category" do
+  test 'should update Category' do
     visit admin_category_url(@admin_category)
-    click_on "Edit this category", match: :first
+    click_on 'Edit this category', match: :first
 
-    fill_in "Description", with: @admin_category.description
-    fill_in "Name", with: @admin_category.name
-    click_on "Update Category"
+    fill_in 'Description', with: @admin_category.description
+    fill_in 'Name', with: @admin_category.name
+    click_on 'Update Category'
 
-    assert_text "Category was successfully updated"
-    click_on "Back"
+    assert_text 'Category was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Category" do
+  test 'should destroy Category' do
     visit admin_category_url(@admin_category)
-    click_on "Destroy this category", match: :first
+    click_on 'Destroy this category', match: :first
 
-    assert_text "Category was successfully destroyed"
+    assert_text 'Category was successfully destroyed'
   end
 end
